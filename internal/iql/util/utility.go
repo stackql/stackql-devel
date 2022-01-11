692858299e7344b8e0cfef40f201e0e5345b1c9a
General Kroll <generalkroll0@gmail.com>
2022-01-11 19:29:21 +1100

    attempt-windows-fix

--- internal/iql/util/utility.go
+++ internal/iql/util/utility.go
@@ -3,12 +3,14 @@ package util
 import (
 	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 func GetFilePathFromRepositoryRoot(relativePath string) (string, error) {
 	_, filename, _, _ := runtime.Caller(0)
 	curDir := filepath.Dir(filename)
-	return filepath.Abs(filepath.Join(curDir, "../../..", relativePath))
+	rv, err := filepath.Abs(filepath.Join(curDir, "../../..", relativePath))
+	return strings.ReplaceAll(rv, `\`, `\\`), err
 }
 
 func MaxMapKey(numbers map[int]interface{}) int {
