61eddbd491054dd136bfa42cbbee46084e5de5a8
General Kroll <generalkroll0@gmail.com>
2022-02-26 22:45:52 +1100

    tests-expanding

--- test/functional/stackql_from_cmd_line.robot
+++ test/functional/stackql_from_cmd_line.robot
@@ -11,9 +11,14 @@ Get StackQL Help
     Should contain    ${result.stdout}   stackql${SPACE}\[command\]
 
 
-Get StackQL Providers
+Get Providers
     ${result} =     Run Process    ${STACKQL_EXE}    exec    \-\-registry\=${RESISTRY_CFG_STR}    ${SHOW_PROVIDERS_STR} 
     Log    ${result.stdout}
     Should contain    ${result.stdout}   okta
+
+Get Okta Services
+    ${result} =     Run Process    ${STACKQL_EXE}    exec    \-\-registry\=${RESISTRY_CFG_STR}    ${SHOW_OKTA_SERVICES_FILTERED_STR} 
+    Log    ${result.stdout}
+    Should contain    ${result.stdout}   Application${SPACE}API
 
 
--- test/functional/variables/stackql_context.py
+++ test/functional/variables/stackql_context.py
@@ -6,7 +6,7 @@ import os
 REPOSITORY_ROOT = os.path.abspath(os.path.join(__file__, "../../../..")).replace("\\","/")
 REGISTRY_ROOT   = os.path.join(REPOSITORY_ROOT, "test/registry").replace("\\","/")
 STACKQL_EXE     = os.path.join(REPOSITORY_ROOT, "build/stackql").replace("\\","/")
-RESISTRY_CFG    = { 
+_RESISTRY_CFG    = { 
   "url": f"file://{REGISTRY_ROOT}",
   "localDocRoot": f"{REGISTRY_ROOT}",
   "useEmbedded": False,
@@ -14,5 +14,6 @@ RESISTRY_CFG    = {
     "nopVerify": True 
   } 
 }
-RESISTRY_CFG_STR = json.dumps(RESISTRY_CFG)
+RESISTRY_CFG_STR = json.dumps(_RESISTRY_CFG)
 SHOW_PROVIDERS_STR = "show providers;"
+SHOW_OKTA_SERVICES_FILTERED_STR  = "show services from okta like 'app%';"
