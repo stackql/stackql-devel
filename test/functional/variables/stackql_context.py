0ff8f32d3e62ba3e312e6ac94fcbce9b14f4b759
General Kroll <generalkroll0@gmail.com>
2022-03-01 11:01:17 +1100

    robot-expanding

--- test/functional/variables/stackql_context.py
+++ test/functional/variables/stackql_context.py
@@ -2,17 +2,22 @@
 import json
 import os
 
+REPOSITORY_ROOT = os.path.abspath(os.path.join(__file__, '..', '..', '..', '..'))
 
 def get_output_from_local_file(fp :str) -> str:
   with open(os.path.join(REPOSITORY_ROOT, fp), 'r') as f:
     return f.read().strip()
 
-REPOSITORY_ROOT = os.path.abspath(os.path.join(__file__, "../../../..")).replace("\\","/")
-REGISTRY_ROOT   = os.path.join(REPOSITORY_ROOT, "test/registry-mocked").replace("\\","/")
-STACKQL_EXE     = os.path.join(REPOSITORY_ROOT, "build/stackql").replace("\\","/")
+def get_unix_path(pathStr :str) -> str:
+  return pathStr.replace('\\', '/')
+
+
+REPOSITORY_ROOT_UNIX = get_unix_path(REPOSITORY_ROOT)
+REGISTRY_ROOT   = get_unix_path(os.path.join(REPOSITORY_ROOT, 'test', 'registry-mocked'))
+STACKQL_EXE     = get_unix_path(os.path.join(REPOSITORY_ROOT, 'build', 'stackql'))
 _REGISTRY_CFG    = { 
-  "url": f"file://{REGISTRY_ROOT}",
-  "localDocRoot": f"{REGISTRY_ROOT}",
+  "url": f"file://{get_unix_path(REGISTRY_ROOT)}",
+  "localDocRoot": f"{get_unix_path(REGISTRY_ROOT)}",
   "useEmbedded": False,
   "verifyConfig": {
     "nopVerify": True 
@@ -20,7 +25,7 @@ _REGISTRY_CFG    = {
 }
 _AUTH_CFG={ 
   "google": { 
-    "credentialsfilepath": f"{REPOSITORY_ROOT}/test/assets/credentials/dummy/google/functional-test-dummy-sa-key.json",
+    "credentialsfilepath": get_unix_path(os.path.join(REPOSITORY_ROOT, 'test', 'assets', 'credentials', 'dummy', 'google', 'functional-test-dummy-sa-key.json')),
     "type": "service_account"
   }, 
   "okta": { 
@@ -29,7 +34,7 @@ _AUTH_CFG={
   } 
 }
 
-with open(f"{REPOSITORY_ROOT}/test/assets/credentials/dummy/okta/api-key.txt", 'r') as f:
+with open(os.path.join(REPOSITORY_ROOT, 'test', 'assets', 'credentials', 'dummy', 'okta', 'api-key.txt'), 'r') as f:
     OKTA_SECRET_STR = f.read()
 
 REGISTRY_CFG_STR = json.dumps(_REGISTRY_CFG)
@@ -39,16 +44,15 @@ SHOW_OKTA_SERVICES_FILTERED_STR  = "show services from okta like 'app%';"
 SHOW_OKTA_APPLICATION_RESOURCES_FILTERED_STR  = "show resources from okta.application like 'gr%';"
 JSON_INIT_FILE_PATH = os.path.join(REPOSITORY_ROOT, 'test', 'server', 'expectations', 'static-gcp-expectations.json')
 MOCKSERVER_JAR = os.path.join(REPOSITORY_ROOT, 'test', 'downloads', 'mockserver-netty-5.12.0-shaded.jar')
-# MOCKSERVER_JAR = '/usr/local/lib/mockserver/mockserver-netty-jar-with-dependencies.jar'
 
 
 SELECT_CONTAINER_SUBNET_AGG_DESC = "select ipCidrRange, sum(5) cc  from  google.container.`projects.aggregated.usableSubnetworks` where projectsId = 'testing-project' group by \"ipCidrRange\" having sum(5) >= 5 order by ipCidrRange desc;"
 SELECT_CONTAINER_SUBNET_AGG_ASC = "select ipCidrRange, sum(5) cc  from  google.container.`projects.aggregated.usableSubnetworks` where projectsId = 'testing-project' group by \"ipCidrRange\" having sum(5) >= 5 order by ipCidrRange asc;"
 
-SELECT_CONTAINER_SUBNET_AGG_DESC_EXPECTED = get_output_from_local_file("test/assets/expected/aggregated-select/google/container/agg-subnetworks-allowed/table/simple-count-grouped-variant-desc.txt")
+SELECT_CONTAINER_SUBNET_AGG_DESC_EXPECTED = get_output_from_local_file(os.path.join('test', 'assets', 'expected', 'aggregated-select', 'google', 'container', 'agg-subnetworks-allowed', 'table', 'simple-count-grouped-variant-desc.txt'))
 
-SELECT_CONTAINER_SUBNET_AGG_ASC_EXPECTED = get_output_from_local_file("test/assets/expected/aggregated-select/google/container/agg-subnetworks-allowed/table/simple-count-grouped-variant-asc.txt")
+SELECT_CONTAINER_SUBNET_AGG_ASC_EXPECTED = get_output_from_local_file(os.path.join('test', 'assets', 'expected', 'aggregated-select', 'google', 'container', 'agg-subnetworks-allowed', 'table', 'simple-count-grouped-variant-asc.txt'))
 
 GET_IAM_POLICY_AGG_ASC_INPUT_FILE = os.path.join(REPOSITORY_ROOT, 'test', 'assets', 'input', 'select-exec-dependent-org-iam-policy.iql')
 
-GET_IAM_POLICY_AGG_ASC_EXPECTED = get_output_from_local_file("test/assets/expected/aggregated-select/google/cloudresourcemanager/select-exec-getiampolicy-agg.csv")
+GET_IAM_POLICY_AGG_ASC_EXPECTED = get_output_from_local_file(os.path.join('test', 'assets', 'expected', 'aggregated-select', 'google', 'cloudresourcemanager', 'select-exec-getiampolicy-agg.csv'))
