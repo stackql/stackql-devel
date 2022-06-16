1e17cc39d1b8cdb245140ed5551916f100073cd8
General Kroll <generalkroll0@gmail.com>
2022-06-16 11:18:50 +1000

    busted-windows-troubleshoot

--- test/robot/lib/StackQLInterfaces.py
+++ test/robot/lib/StackQLInterfaces.py
@@ -4,10 +4,14 @@ from robot.libraries.BuiltIn import BuiltIn
 
 from robot.libraries.Process import Process
 
+from robot.libraries.OperatingSystem import OperatingSystem 
+
+import os
+
 
 
 @library(scope='SUITE', version='0.1.0', doc_format='reST')
-class StackQLInterfaces(Process, BuiltIn):
+class StackQLInterfaces(OperatingSystem, Process, BuiltIn):
   ROBOT_LISTENER_API_VERSION = 2
 
   def __init__(self):
@@ -39,6 +43,31 @@ class StackQLInterfaces(Process, BuiltIn):
     self.log(result.stdout)
     self.log(result.stderr)
     return result
+
+  def _run_stackql_exec_command(
+    self,  
+    stackql_exe :str, 
+    okta_secret_str :str,
+    github_secret_str :str,
+    k8s_secret_str :str,
+    registry_cfg_str :str, 
+    auth_cfg_str :str, 
+    query
+  ):
+    self.set_environment_variable("OKTA_SECRET_KEY", okta_secret_str)
+    self.set_environment_variable("GITHUB_SECRET_KEY", github_secret_str)
+    self.set_environment_variable("K8S_SECRET_KEY", k8s_secret_str)
+    res = super().run_process(
+      stackql_exe,
+      "exec",
+      f"--registry={registry_cfg_str}",
+      f"--auth={auth_cfg_str}",
+      "--tls.allowInsecure=true",
+      query
+    )
+    self.log(res.stdout)
+    self.log(res.stderr)
+    return res
   
   @keyword
   def should_PG_client_error_inline_contain(self, curdir :str, psql_exe :str, psql_conn_str :str, query :str, expected_output :str):
@@ -67,5 +96,53 @@ class StackQLInterfaces(Process, BuiltIn):
       psql_exe,
       psql_conn_str,
       query
+    )
+    return self.should_be_equal(result.stdout, expected_output)
+  
+  @keyword
+  def should_stackql_inline_equal(
+    self, 
+    stackql_exe :str, 
+    okta_secret_str :str,
+    github_secret_str :str,
+    k8s_secret_str :str,
+    registry_cfg_str :str, 
+    auth_cfg_str :str, 
+    query :str,
+    expected_output :str
+  ):
+    result = self._run_stackql_exec_command(
+      stackql_exe, 
+      okta_secret_str,
+      github_secret_str,
+      k8s_secret_str,
+      registry_cfg_str, 
+      auth_cfg_str, 
+      query,
+    )
+    return self.should_be_equal(result.stdout, expected_output)
+
+  @keyword
+  def should_horrid_query_stackql_inline_equal(
+    self, 
+    stackql_exe :str, 
+    okta_secret_str :str,
+    github_secret_str :str,
+    k8s_secret_str :str,
+    registry_cfg_str :str, 
+    auth_cfg_str :str,
+    expected_output :str
+  ):
+    _SELECT_GOOGLE_JOIN_CONCATENATED_SELECT_EXPRESSIONS :str =  """SELECT i.zone, i.name, i.machineType, i.deletionProtection, '[{"subnetwork":"', JSON_EXTRACT(i.networkInterfaces, '$[0].subnetwork'), '"}]', '[{"boot": true, "initializeParams": { "diskSizeGb": "', JSON_EXTRACT(i.disks, '$[0].diskSizeGb'), '", "sourceImage": "', d.sourceImage, '"}}]', i.labels FROM google.compute.instances i INNER JOIN google.compute.disks d ON i.name = d.name WHERE i.project = 'testing-project' AND i.zone = 'australia-southeast1-a' AND d.project = 'testing-project' AND d.zone = 'australia-southeast1-a' AND i.name LIKE '%' order by i.name DESC;"""
+    if os.name == 'nt':
+      _SELECT_GOOGLE_JOIN_CONCATENATED_SELECT_EXPRESSIONS =  SELECT_GOOGLE_JOIN_CONCATENATED_SELECT_EXPRESSIONS.replace('"', '""')
+    result = self._run_stackql_exec_command(
+      stackql_exe, 
+      okta_secret_str,
+      github_secret_str,
+      k8s_secret_str,
+      registry_cfg_str, 
+      auth_cfg_str, 
+      str.encode(_SELECT_GOOGLE_JOIN_CONCATENATED_SELECT_EXPRESSIONS)
     )
     return self.should_be_equal(result.stdout, expected_output)
