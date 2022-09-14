c6faeb8980d8cc5cc21b22516f6c09937e64355c
General Kroll <generalkroll0@gmail.com>
2022-09-14 12:13:17 +1000

    tests-working

--- internal/stackql/output/output.go
+++ internal/stackql/output/output.go
@@ -288,6 +288,9 @@ func decodeRow(colz []sqldata.ISQLColumn, row sqldata.ISQLRow, ci *pgtype.ConnIn
 	var retVal [][]byte
 	rawRow := row.GetRowDataNaive()
 	if len(rawRow) != len(colz) {
+		if len(rawRow) == 0 {
+			return nil, nil
+		}
 		return nil, fmt.Errorf("row length != column count (%d != %d)", len(rawRow), len(colz))
 	}
 	for i, col := range colz {
@@ -307,6 +310,9 @@ func tabulateResults(r sqldata.ISQLResult, ci *pgtype.ConnInfo) ([][]string, err
 		rd, err := decodeRow(colz, v, ci)
 		if err != nil {
 			return nil, err
+		}
+		if rd == nil {
+			continue
 		}
 		var rs []string
 		for _, b := range rd {
