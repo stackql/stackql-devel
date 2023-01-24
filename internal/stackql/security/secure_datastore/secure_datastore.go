e9fd64395ee982b15414fd300a78908165f7ffae
General Kroll <generalkroll0@gmail.com>
2023-01-24 20:10:28 +1100

    correct-premature-activity

--- internal/stackql/security/secure_datastore/secure_datastore.go
+++
@@ -1,9 +0,0 @@
-package secure_datastore
-
-type SecureKVDataStore interface {
-	Get(string) (map[string]interface{}, error)
-	Put(string, map[string]interface{}) (map[string]interface{}, error)
-}
-
-type passwordProtectedSecureKVDataStore struct {
-}
