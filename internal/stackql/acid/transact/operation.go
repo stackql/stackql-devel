72778b3c7b82230a9bc031972c7f96db591806f8
General Kroll <generalkroll0@gmail.com>
2023-03-26 17:54:29 +1100

    api-changes

--- internal/stackql/acid/transact/operation.go
+++ internal/stackql/acid/transact/operation.go
@@ -3,6 +3,7 @@ package transact
 import (
 	"fmt"
 
+	"github.com/stackql/stackql/internal/stackql/internal_data_transfer/internaldto"
 	"github.com/stackql/stackql/internal/stackql/primitive"
 )
 
@@ -24,25 +25,42 @@ var (
 // node alongside log entries.
 type Operation interface {
 	// Execute the operation.
-	Execute() error
+	Execute(primitive.IPrimitiveCtx) internaldto.ExecutorOutput
 	// Reverse the operation.
 	Undo() error
 	// Get the redo log entry.
 	GetRedoLog() (LogEntry, bool)
 	// Get the undo log entry.
 	GetUndoLog() (LogEntry, bool)
+	//
+	IncidentData(int64, internaldto.ExecutorOutput) error
+	//
+	SetTxnID(id int)
+	//
+	SetInputAlias(alias string, id int64) error
 }
 
-//nolint:unused // under construction
 type reversibleOperation struct {
 	redoLog LogEntry
 	undoLog LogEntry
 	pr      primitive.IPrimitive
-	pc      primitive.IPrimitiveCtx
 }
 
-func (op *reversibleOperation) Execute() error {
-	return nil
+func (op *reversibleOperation) Execute(pc primitive.IPrimitiveCtx) internaldto.ExecutorOutput {
+	rv := op.pr.Execute(pc)
+	return rv
+}
+
+func (op *reversibleOperation) IncidentData(id int64, ou internaldto.ExecutorOutput) error {
+	return op.pr.IncidentData(id, ou)
+}
+
+func (op *reversibleOperation) SetTxnID(id int) {
+	op.pr.SetTxnID(id)
+}
+
+func (op *reversibleOperation) SetInputAlias(alias string, id int64) error {
+	return op.pr.SetInputAlias(alias, id)
 }
 
 func (op *reversibleOperation) Undo() error {
@@ -63,13 +81,24 @@ func (op *reversibleOperation) GetUndoLog() (LogEntry, bool) {
 
 type irreversibleOperation struct {
 	redoLog LogEntry
-	pc      primitive.IPrimitiveCtx
 	pr      primitive.IPrimitive
 }
 
-func (op *irreversibleOperation) Execute() error {
-	res := op.pr.Execute(op.pc)
-	return res.GetError()
+func (op *irreversibleOperation) Execute(pc primitive.IPrimitiveCtx) internaldto.ExecutorOutput {
+	res := op.pr.Execute(pc)
+	return res
+}
+
+func (op *irreversibleOperation) IncidentData(id int64, ou internaldto.ExecutorOutput) error {
+	return op.pr.IncidentData(id, ou)
+}
+
+func (op *irreversibleOperation) SetTxnID(id int) {
+	op.pr.SetTxnID(id)
+}
+
+func (op *irreversibleOperation) SetInputAlias(alias string, id int64) error {
+	return op.pr.SetInputAlias(alias, id)
 }
 
 func (op *irreversibleOperation) Undo() error {
@@ -86,4 +115,10 @@ func (op *irreversibleOperation) GetRedoLog() (LogEntry, bool) {
 
 func (op *irreversibleOperation) GetUndoLog() (LogEntry, bool) {
 	return nil, false
+}
+
+func NewIrreversibleOperation(pr primitive.IPrimitive) Operation {
+	return &irreversibleOperation{
+		pr: pr,
+	}
 }
