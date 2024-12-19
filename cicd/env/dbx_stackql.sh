c5c089ff46c05f83f1d65a0e98bf2e46cb41097b
General Kroll <generalkroll0@gmail.com>
2024-12-19 14:32:50 +1100

    disable cache

---
+++ cicd/env/dbx_stackql.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export DATABRICKS_ACCOUNT_ID=ebfcc5a9-9d49-4c93-b651-b3ee6cf1c9ce
+export DATABRICKS_CLIENT_ID=0b7b23de-3e7d-4432-812c-cf517e079a22
+export DATABRICKS_CLIENT_SECRET=dose01fa285777be67724b58a079c0b27b75
+
+
+# select * from databricks_account.provisioning.workspaces where account_id = 'ebfcc5a9-9d49-4c93-b651-b3ee6cf1c9ce';
